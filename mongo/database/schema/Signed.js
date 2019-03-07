49ff12f832d3095af61d7c87a964740b435b7553
bs <228713680@qq.com>
2019-03-07 22:39:52 +0800

    签到表关联用户表

--- mongo/database/schema/Signed.js
+++ mongo/database/schema/Signed.js
@@ -4,7 +4,7 @@ let ObjectId = Schema.Types.ObjectId
 
 const signedSchema = new Schema({
   id: ObjectId,
-  userId: { type: String, required: true },
+  userId: { type: ObjectId, ref: 'User' },
   times: { type: String, default: '1' },
   todayIsSigned: { type: Boolean, default: true },
   signedAt: { type: Date, default: Date.now() }
