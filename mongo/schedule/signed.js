06fe33f9a209226e4b833c68510af0cb94472187
bs <228713680@qq.com>
2019-03-07 18:57:16 +0800

    设置定时任务为每天的0点

--- mongo/schedule/signed.js
+++ mongo/schedule/signed.js
@@ -2,7 +2,7 @@ let schedule = require('node-schedule')
 let mongoose = require('mongoose')
 
 function resetSigned () {
-  schedule.scheduleJob('30 * * * * *', async () => {
+  schedule.scheduleJob('00 0 0 * * *', async () => {
     let Signed = mongoose.model('Signed')
     await Signed.update(null, { todayIsSigned: false }, { multi: true }).then(res => {
       console.log('--------------')
