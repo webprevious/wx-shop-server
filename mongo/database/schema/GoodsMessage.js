2fc286e1f6656e03d32393aa13a60e81157f069a
bs <228713680@qq.com>
2019-03-10 00:58:48 +0800

    发布物品进行分类id和发布者id进行关联

--- mongo/database/schema/GoodsMessage.js
+++ mongo/database/schema/GoodsMessage.js
@@ -4,17 +4,19 @@ let ObjectId = Schema.Types.ObjectId
 
 const GoodsMessageSchema = new Schema({
   GoodsId: ObjectId,
-  publisherId: { required: true, type: String},
+  publisherId: { required: true, type: String, ref: 'User'},
   goodsTitle: { required: true, type: String},
   goodsPrice: { required: true, type: Number},
   goodsDetailMsg: { required: true, type: String},
-  goodsPics: { required: true, type: Array},
-  goodsCategoryId: { required: true, type: String},
+  goodsPics: { required: true, type: Array, default: [] },
+  goodsCategoryId: { required: true, type: String, ref: 'GoodsCategory'},
   goodsBussPath: { required: true, type: Array},
   goodsConnWx: String,
   goodsConnQq: String,
   goodsConnPhone: String,
   goodsStatus: { default: 'wait_verify', type: String},
+  goodsViewTimes: { default: 0, type: Number },
+  publishAt: { type: Date, default: Date.now() }
 })
 
 mongoose.model('PublishGoods', GoodsMessageSchema)
