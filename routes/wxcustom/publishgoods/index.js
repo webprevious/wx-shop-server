3f00940923b041f950921099a8280b5af25528af
bs <228713680@qq.com>
2019-03-11 00:13:41 +0800

    记录浏览次数接口，根据物品id获取物品详情接口

--- routes/wxcustom/publishgoods/index.js
+++ routes/wxcustom/publishgoods/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 // 解决使用findByIdAndUpdate警告问题，useFindAndModify默认为true，如果使用findAnd这种多操作的方法，mongoose会优先使用FindAndModify导致警告，设置为false他就会原生调用你所写的方法
 mongoose.set('useFindAndModify', false)
 
+// 发布物品和修改物品接口
 router.post('/publishGoods', async (ctx, next) => {
   const PublishGoods = mongoose.model('PublishGoods')
   // 发布物品和修改物品共用一个接口，以是否传了物品id作为区别
@@ -44,6 +45,47 @@ router.post('/publishGoods', async (ctx, next) => {
       }
     })
   }
+})
+
+// 记录浏览次数
+router.post('/addGoodsViewTimes', async ctx => {
+  let reqData = ctx.request.body
+  let PublishGoods = mongoose.model('PublishGoods')
+  await PublishGoods.findById(reqData.goodsId).then(async res => {
+    res.goodsViewTimes ++
+    await res.save().then(res => {
+      ctx.body = {
+        code: 1,
+        data: res
+      }
+    }).catch(err => {
+      ctx.body = {
+        code: 0,
+        data: err
+      }
+    })
+  }).catch(err => {
+    ctx.body = {
+      code: 0,
+      data: err
+    }
+  })
+})
+
+// 根据物品id获取物品详情
+router.get('/getGoodsDetailById', async ctx => {
+  let PublishGoods = mongoose.model('PublishGoods')
+  await PublishGoods.findById(ctx.query.goodsId).then(res => {
+    ctx.body = {
+      code: 1,
+      data: res
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 0,
+      data: err
+    }
+  })
 })
 
 module.exports = router
