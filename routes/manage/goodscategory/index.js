17eee19bc7fc859f1c2777ad8171b93c24be44a0
bs <228713680@qq.com>
2019-03-11 16:48:23 +0800

    优化分类添加接口，添加分类之前先进行判断是否有该分类

--- routes/manage/goodscategory/index.js
+++ routes/manage/goodscategory/index.js
@@ -5,15 +5,25 @@ router.post('/addGoodsCateory', async (ctx, next) => {
   // 获取modle
   const GoodsCategory = mongoose.model('GoodsCategory')
   // create存操作方法不需要创建保存实例，存入字段需要跟前端传入对应
-  await GoodsCategory.create(ctx.request.body).then(res => {
-    ctx.body = {
-      code: 1,
-      data: res
-    }
-  }).catch(err => {
-    ctx.body = {
-      code: 0,
-      data: err
+  let reqData = ctx.request.body
+  await GoodsCategory.findOne({ title: reqData.title}).then(async res => {
+    if (res) {
+      ctx.body = {
+        code: 1,
+        data: '该分类已存在'
+      }
+    } else {
+      await GoodsCategory.create(reqData).then(res => {
+        ctx.body = {
+          code: 1,
+          data: res
+        }
+      }).catch(err => {
+        ctx.body = {
+          code: 0,
+          data: err
+        }
+      })
     }
   })
 })
