cdde798206597959807d83da37224d0aa948f1c6
bs <228713680@qq.com>
2019-03-12 18:55:53 +0800

    搜索接口开发、猜你喜欢接口开发，优化查询接口

--- routes/wxcustom/goodscategory/index.js
+++ routes/wxcustom/goodscategory/index.js
@@ -25,7 +25,7 @@ router.post('/getGoodsByCategoryId', async ctx => {
   // 如果是1那就是请求推荐
   // 获取推荐tab下的物品，目前没有加入算法和复杂计算，只是找出数据库中所有数据
   if (reqData.goodsCategoryId === '1') {
-    await GoodsMessage.find({goodsStatus: 'pass_verify'}, {goodsFirstPic: 1, goodsTitle: 1, goodsPrice: 1, goodsViewTimes: 1}).then(res => {
+    await GoodsMessage.find({goodsStatus: 'pass_verify'}).populate('publisherId').then(res => {
       ctx.body = {
         code: 1,
         data: res
@@ -37,7 +37,7 @@ router.post('/getGoodsByCategoryId', async ctx => {
       }
     })
   } else {
-    await GoodsMessage.find({goodsCategoryId: reqData.goodsCategoryId, goodsStatus: 'pass_verify'}, {goodsFirstPic: 1, goodsTitle: 1, goodsPrice: 1, goodsViewTimes: 1}).then(res => {
+    await GoodsMessage.find({goodsCategoryId: reqData.goodsCategoryId, goodsStatus: 'pass_verify'}).populate('publisherId').then(res => {
       if (res) {
         ctx.body = {
           code: 1,
@@ -56,6 +56,55 @@ router.post('/getGoodsByCategoryId', async ctx => {
       }
     })
   }
+})
+
+// 猜你喜欢，目前只是随机出现2个
+router.post('/getUserLove', async ctx => {
+  const GoodsMessage = mongoose.model('GoodsMessage')
+  await GoodsMessage.find({goodsStatus: 'pass_verify'}).populate('publisherId').then(res => {
+    if (res.length <= 2) {
+      ctx.body = {
+        code: 1,
+        data: res
+      }
+    } else {
+      // 要取得个数
+      let num = 2
+      let result = []
+      for (let i = 0; i < num; i++) {
+        let ran = Math.floor(Math.random() * res.length)
+        result.push(res.splice(ran, 1)[0])
+      }
+      ctx.body = {
+        code: 1,
+        data: result
+      }
+    }
+  })
+})
+
+// 搜索接口，模糊查询
+router.post('/search', async ctx => {
+  const GoodsMessage = mongoose.model('GoodsMessage')
+  let reqData = ctx.request.body
+  await GoodsMessage.find({goodsTitle: {'$regex': reqData.keyword},goodsStatus: 'pass_verify'}).populate('publisherId').then(res => {
+    if (res.length) {
+      ctx.body = {
+        code: 1,
+        data: res
+      }
+    } else {
+      ctx.body = {
+        code: 0,
+        data: res
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 0,
+      data: err
+    }
+  })
 })
 
 module.exports = router
