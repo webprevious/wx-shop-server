1ae3bc9a7d3196ff6226c6cc6d467669bcdb3b09
gy <228713680@qq.com>
2019-03-30 22:59:30 +0800

    管理端登录接口

---
+++ routes/manage/login/index.js
@@ -0,0 +1,46 @@
+const router = require('koa-router')()
+const mongoose = require('mongoose')
+
+// 登录接口
+router.post('/manLogin', async ctx => {
+  let ManageUser = mongoose.model('ManageUser')
+  let reqData = ctx.request.body
+  await ManageUser.findOne({userName: reqData.userName}).then(res => {
+    console.log(res)
+    // 如果用户名不存在，那么res为null
+    if (res) {
+      // 判断密码是否正确
+      if (res.password === reqData.password) {
+        delete res.password
+        console.log(res)
+        ctx.body = {
+          code: 1,
+          data: {
+            createAt: res.createAt,
+            userName: res.userName,
+            role: res.role,
+            isActive: res.isActive
+          }
+        }
+      } else {
+        ctx.body = {
+          code: 0,
+          data: '密码错误'
+        }
+      }
+    } else {
+      ctx.body = {
+        code: 0,
+        data: '用户名不存在'
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 0,
+      data: '登录失败，请稍后再试',
+      msg: err
+    }
+  })
+})
+
+module.exports = router
