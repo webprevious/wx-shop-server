f663ca692ce3f81b1e340b584c8a2bfb7aadcb0d
gy <228713680@qq.com>
2019-04-10 11:57:18 +0800

    获取轮播图列表时带回广告物品的信息

--- routes/wxcustom/adcarousel/index.js
+++ routes/wxcustom/adcarousel/index.js
@@ -5,7 +5,7 @@ router.get('/getAdCarousel', async (ctx, next) => {
   // 获取modle
   const AdCarousel = mongoose.model('AdCarousel')
   // 直接使用model进行查询，查询不需要创建保存实例
-  await AdCarousel.find().then(result => {
+  await AdCarousel.find({isActive: true}).populate({path: 'goodsId', populate: { path: 'publisherId'}}).then(result => {
     ctx.body = {
       code: 1,
       data: result
